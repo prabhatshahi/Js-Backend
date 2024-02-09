893fe9aae040b788bec39622b87c716868bd4208
Prabhat Ranjan Shahi <ranjanprabhat555@gmail.com>
2024-02-09 19:32:57 +0530

    connected database

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
+// required("dotenv").config({ path: "./env" });
+
+import dotenv from "dotenv";
+
 import mongoose from "mongoose";
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+
+/*import express from "express";
+const app = express(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+        app.on("error", (err) => {
+            console.log("Error", err);
+            throw err;
+        });
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listing on port ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.log("Error", error);
+        throw err;
+    }
+})();*/
