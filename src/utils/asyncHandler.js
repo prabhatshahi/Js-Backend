29eace8529292a3671f9b2276588e62a17a1efea
Prabhat Ranjan Shahi <ranjanprabhat555@gmail.com>
2024-02-10 10:19:19 +0530

    utils code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+const asyncHandler = (requestHandler) => {
+  return async (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// const asyncHanler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//      // next() is used to call the next middleware function in the stack. If there are no more middleware functions in the stack, the request will be handled by the default Express error handling middleware. This is useful for handling errors that occur in asynchronous code.
+
+//     // if there is no next() then the request will be handled by the default Express error handling middleware. This is useful for handling errors that occur in asynchronous code.
+//     // if there is no next() then the request will be handled by the default Express error handling middleware. This is useful for handling errors that occur in asynchronous code.
+//     // if there is no next() then the request will be handled by the default Express error handling middleware. This is useful for handling errors that occur in asynchronous code.
+//     // if there is no next() then the request will be handled by the default Express error handling middleware. This is useful for handling errors that occur in asynchronous code.
+//     // if there is no next() then the request will be handled by the default Express error handling middleware. This is useful for handling errors that occur in asynchronous code.
+//     // if there is no next() then the request will be
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
